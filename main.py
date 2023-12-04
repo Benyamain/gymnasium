bdde476d899737a257267e945152cd51608228ec
Benyamain Yacoob <yacoobbenyamain@gmail.com>
2023-12-03 20:36:46 -0500

    more figures to see the simulations

--- main.py
+++ main.py
@@ -111,3 +111,59 @@ training_error_moving_avg = (np.convolve(np.array(agent.training_error), np.ones
 axs[2].plot(range(len(training_error_moving_avg)), training_error_moving_avg)
 plt.tight_layout()
 plt.show()
+
+# visualizing the policy
+def create_grids(agent, usable_ace=False):
+    state_value = defaultdict(float)
+    policy = defaultdict(int)
+    for obs, action_values in agent.q_values.items():
+        state_value[obs] = float(np.max(action_values))
+        policy[obs] = int(np.argmax(action_values))
+    
+    player_count, dealer_count = np.meshgrid(np.arange(12, 22), np.arange(1, 11))
+
+    # value grid for plotting
+    value = np.apply_along_axis(lambda obs: state_value[(obs[0], obs[1], usable_ace)], axis=2, arr=np.dstack([player_count, dealer_count]))
+    value_grid = player_count, dealer_count, value
+
+    # create policy for plotting
+    policy_grid = np.apply_along_axis(lambda obs: policy[(obs[0], obs[1], usable_ace)], axis=2, arr=np.dstack([player_count, dealer_count]))
+
+    return value_grid, policy_grid
+
+def create_plots(value_grid, policy_grid, title: str):
+    player_count, dealer_count, value = value_grid
+    fig = plt.figure(figsize=plt.figaspect(0.4))
+    fig.suptitle(title, fontsize=16)
+
+    # plot state values
+    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    ax1.plot_surface(player_count, dealer_count, value, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    plt.xticks(range(12, 22), range(12, 22))
+    plt.yticks(range(1, 11), ['A'] + list(range(2, 11)))
+    ax1.set_title(f"State Values: {title}")
+    ax1.set_xlabel("Player Sum")
+    ax1.set_ylabel("Dealer Showing")
+    ax1.zaxis.set_rotate_label(False)
+    ax1.set_zlabel("Value", fontsize=14, rotation=90)
+    ax1.view_init(20, 220)
+
+    # plot the policy
+    fig.add_subplot(1, 2, 2)
+    ax2 = sns.heatmap(policy_grid, linewidth=0, annot=True, cmap='Accent_r', cbar=False)
+    ax2.set_title(f"Policy: {title}")
+    ax2.set_xlabel("Player Sum")
+    ax2.set_ylabel("Dealer Showing")
+    ax2.set_xticklabels(range(12, 22))
+    ax2.set_yticklabels(['A'] + list(range(2, 11)), fontsize=12)
+
+    # legend
+    legent_elements = [Patch(facecolor='lightgreen', edgecolor='black', label='Hit'), Patch(facecolor='grey', edgecolor='black', label='Stick')]
+    ax2.legend(handles=legent_elements, bbox_to_anchor=(1.3,1))
+    
+    return fig
+
+# state values & policy with usable ace
+value_grid, policy_grid = create_grids(agent, usable_ace=True)
+fig1 = create_plots(value_grid, policy_grid, title='With Usable Ace')
+plt.show()
